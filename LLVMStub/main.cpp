f5d8205311da54a153b4c963192c8b4223bf21ea
genuine <blaquee@gmail.com>
2017-01-26 11:17:50 -0500

    fix project settings

---
+++ LLVMStub/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <Windows.h>
+
+
+void get_image_base()
+{
+	
+}
+int main(int argc, char** argv)
+{
+	
+}
--- LLVMStub/typedefs.h
+++ LLVMStub/typedefs.h
@@ -2,4 +2,9 @@
 #define _TYPEDEF_
 
 
+typedef HMODULE(WINAPI *pLoadLibraryA)(LPCTSTR lpFileName);
+typedef FARPROC(WINAPI *pGetProcAddress)(HMODULE hModule, LPCSTR lpProcName);
+
+typedef LPVOID(WINAPI *pVirtualAlloc)(LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
+typedef BOOL(WINAPI *pVirtualProtect)(LPVOID lpAddress, SIZE_T dwSize, DWORD flNewProtect, PDWORD lpflOldProtect);
 #endif // 
